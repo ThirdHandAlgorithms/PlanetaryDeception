0c920bdf7ac75fa27bcc340922cbfb0f9de5f953
partouf <partouf@gmail.com>
2017-01-03 20:14:27 +0100

    resolves #9 basic Inventory/Item classes

---
+++ Assets/scripts/items/KnownItemsInventory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PlanetaryDeception
+{
+    class KnownItemsInventory: ItemInventory
+    {
+        private static KnownItemsInventory ThisInstance = null;
+
+        public const int BubbleGumWrappingPaper = 1;
+
+        public KnownItemsInventory()
+        {
+            Add(new ItemTag(BubbleGumWrappingPaper, ItemClassType.Trash, "Bubble gum wrapping paper"));
+        }
+
+        public static KnownItemsInventory Instance()
+        {
+            if (ThisInstance == null)
+            {
+                ThisInstance = new KnownItemsInventory();
+            }
+
+            return ThisInstance;
+        }
+
+        /// <summary>
+        /// Transfers an Item from this inventory to another one
+        /// </summary>
+        /// <param name="AItemID"></param>
+        /// <param name="ADestinationInventory"></param>
+        public void TransferItem(int AItemID, ItemInventory ADestinationInventory)
+        {
+            var Item = Peek(AItemID);
+            ADestinationInventory.Add(Item);
+        }
+    };
+}
---
+++ Assets/scripts/level1/LevelController_1.cs
@@ -0,0 +1,33 @@
+﻿using PlanetaryDeception;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelController_1 : MonoBehaviour {
+    public Text alertText;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var Inventory = PlayerInventory.Instance();
+
+        if (Input.anyKey)
+        {
+            if (!Inventory.ContainsItem(KnownItemsInventory.BubbleGumWrappingPaper))
+            {
+                var KnownItems = KnownItemsInventory.Instance();
+                KnownItems.TransferItem(KnownItemsInventory.BubbleGumWrappingPaper, Inventory);
+            }
+        }
+
+        if (Inventory.ContainsItem(KnownItemsInventory.BubbleGumWrappingPaper))
+        {
+            alertText.text = "You have BubbleGumWrappingPaper";
+        }
+    }
+}
