1cd4de4796706ca27fcc55d4c325082494cd5a36
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2017-01-03 16:26:32 +0100

    Added IntroController with placeholder Splash and placeholder Level_1

---
+++ Assets/scripts/intro/IntroController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Intro Controller.
+/// Does nothing for now, just waits for key input to launch the game.
+/// </summary>
+public class IntroController : MonoBehaviour
+{
+    /// <summary>
+    /// Update this instance. Press any key..
+    /// </summary>
+    public void Update()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Level_1");
+        }
+    }
+}
